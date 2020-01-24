d2566c9e25ea92d8f95cad7f0a01e378ecbca4f2
Mathias Moreira <viejocadillac@gmail.com>
2020-01-24 02:07:21 -0300

    Updates

---
+++ precache-manifest.ae5d1ed350e77b7c7880ed1f978b7828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba705bd16be68ee761128a62cf172a7",
+    "url": "/React-Sort/index.html"
+  },
+  {
+    "revision": "7f48adbb116a0afd2e5f",
+    "url": "/React-Sort/static/css/main.a460e538.chunk.css"
+  },
+  {
+    "revision": "629561e3be8face4e2d2",
+    "url": "/React-Sort/static/js/2.97548b9e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Sort/static/js/2.97548b9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f48adbb116a0afd2e5f",
+    "url": "/React-Sort/static/js/main.44d6df76.chunk.js"
+  },
+  {
+    "revision": "f96c2055675ead18a7fa",
+    "url": "/React-Sort/static/js/runtime-main.a4227ca3.js"
+  }
+]);
